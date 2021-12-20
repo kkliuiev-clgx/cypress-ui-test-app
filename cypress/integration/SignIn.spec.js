2c39270646cc4ff7796220fc490af0dd9b1b9df4
NikoLazur <lazur.niko@gmail.com>
2021-12-20 17:11:28 +0200

    added sign up test suite & started log in

--- cypress/integration/SignIn.spec.js
+++ cypress/integration/SignIn.spec.js
@@ -1,12 +1,14 @@
 describe('Sign in suite', () => {
-
   let existingUser = {
     "username": "Snow",
     "password": "12345Qwert!"
   };
 
   it('Existing user should be able to login', () => {
-    cy.get();
-  })
-  
-})
+    cy.visit('/')
+    cy.get("button").should("have.attr", "type", "submit");
+    cy.get("#username").type(existingUser.username);
+    cy.get("#password").type(existingUser.password);
+    cy.get().click();
+  });
+});
