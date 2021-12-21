56863a31045cac5aaccdd383cbcd85d5e6e0e559
NikoLazur <lazur.niko@gmail.com>
2021-12-21 16:17:26 +0200

    added sign in test suite

--- cypress/integration/SignIn.spec.js
+++ cypress/integration/SignIn.spec.js
@@ -1,14 +1,55 @@
-describe('Sign in suite', () => {
+/* eslint-disable prettier/prettier */
+const { before } = require("lodash");
+
+describe("Sign in suite", () => {
   let existingUser = {
     "username": "Snow",
     "password": "12345Qwert!"
   };
 
+  beforeEach('Visit url', () => {
+    cy.visit("/signin");
+  });
+
+  it('Sign in page has Logo, Sign in title and Sign up link', () => {
+    cy.get("svg").should("have.attr", "xmlns", "http://www.w3.org/2000/svg").and("be.visible");
+    cy.get('[data-test="signup"]')
+      .should("have.attr", "href", "/signup")
+      .and("contain", "Don't have an account? Sign Up");
+  });
+
   it('Existing user should be able to login', () => {
-    cy.visit('/')
-    cy.get("button").should("have.attr", "type", "submit");
-    cy.get("#username").type(existingUser.username);
-    cy.get("#password").type(existingUser.password);
-    cy.get().click();
+    cy.get('[data-test="signin-submit"]')
+      .click().should("be.disabled");
+
+    cy.get("#username-label")
+      .should("contain", "Username");
+    cy.get("#username")
+      .type(existingUser.username);
+      
+    cy.get("#password-label")
+      .should("contain", "Password");
+    cy.get("#password")
+      .type(existingUser.password);
+
+    cy.contains(".MuiFormControlLabel-root", "Remember me")
+      .find(".PrivateSwitchBase-input-14")
+      .should("have.attr", "type", "checkbox")
+      .and('be.not.checked');
+    cy.get(".PrivateSwitchBase-input-14")
+      .check()
+      .should('be.checked')
+      .uncheck()
+      .should("not.be.checked");
+
+    cy.get('[data-test="signin-submit"]')
+      .find(".MuiButton-label")
+      .should("contain", "Sign In");
+    cy.get('[data-test="signin-submit"]')
+      .should("not.be.disabled");
+    cy.get('[data-test="signin-submit"]')
+      .should("be.not.disabled");
+    cy.get('[data-test="signin-submit"]')
+      .click();
   });
 });
