e6ccedc4d9d530f6a926c99fe61bf196b596fe50
Serikova_Mary <levdan42108@gmail.com>
2022-01-11 06:06:18 +0200

    Mocking data

--- cypress/integration/Serikova_Test_spec.js
+++ cypress/integration/Serikova_Test_spec.js
@@ -1,3 +1,4 @@
+import { userFieldsValidator } from "../../backend/validators";
 import {SignUpPage, SignInPage} from "../page-objects/page";
 
 describe ('Sign Up tests', () => {
@@ -9,28 +10,64 @@ describe ('Sign Up tests', () => {
     })
 
     it('Elements existance', () => {
-        signUpPage.CheckHeaderElementExistence();
-        signUpPage.CheckFieldsExistence();
-        signUpPage.CheckButtonsExistence();
-        signUpPage.clickAllFields();
-        signUpPage.CheckErrorMessagesExistence();
-        signUpPage.CheckErrorMessagesColor();
+        signUpPage.logo().should('exist');
+        signUpPage.signupTitle().should('exist');
+        signUpPage.firstNameField().should('exist');
+        signUpPage.lastNameField().should('exist');
+        signUpPage.usernameField().should('exist');
+        signUpPage.passwordField().should('exist');
+        signUpPage.confirmPasswordField().should('exist');
+        signUpPage.signInLink().should('exist');
+        signUpPage.submitButton().should('exist');
+        signUpPage.firstNameField().click();
+        signUpPage.lastNameField().click();
+        signUpPage.usernameField().click();
+        signUpPage.passwordField().click();
+        signUpPage.confirmPasswordField().click();
+        signUpPage.signInLink().click();
+        signUpPage.firstNameHelperText().should('exist');
+        signUpPage.lastNameHelperText().should('exist');
+        signUpPage.UsernameHelperText().should('exist');
+        signUpPage.passwordHelperText().should('exist');
+        signUpPage.confirmPasswordHelperText().should('exist');
+        signUpPage.firstNameHelperText().should('have.css', 'color').and('eq', 'rgb(244, 67, 54)');
+        signUpPage.lastNameHelperText().should('have.css', 'color').and('eq', 'rgb(244, 67, 54)');
+        signUpPage.UsernameHelperText().should('have.css', 'color').and('eq', 'rgb(244, 67, 54)');
+        signUpPage.passwordHelperText().should('have.css', 'color').and('eq', 'rgb(244, 67, 54)');
+        signUpPage.confirmPasswordHelperText().should('have.css', 'color').and('eq', 'rgb(244, 67, 54)');
     }); 
 
     it('Password match error', () => {
-        signUpPage.InsertData("Password match error");
+        signUpPage.firstNameField().click().type('1234');
+        signUpPage.lastNameField().click().type('1234');
+        signUpPage.usernameField().type('1234');
+        signUpPage.passwordField().click().type('1234');
+        signUpPage.confirmPasswordField().click().type('1111');
+        signUpPage.confirmPasswordHelperText().should('contain', 'Password does not match')
     });
 
     it('Invalid Password error', () => {
-        signUpPage.InsertData("Invalid Password error");
+        signUpPage.firstNameField().click().type('1234');
+        signUpPage.lastNameField().click().type('1234');
+        signUpPage.usernameField().type('1234');
+        signUpPage.passwordField().click().type('123');
+        signUpPage.confirmPasswordField().click().type('123');
+        signUpPage.passwordHelperText().should('contain', 'Password must contain at least 4 characters')
     });
 
     it('Registration', () => {
-        signUpPage.InsertData("Registration");
+        signUpPage.firstNameField().click().type('1234');
+        signUpPage.lastNameField().click().type('1234');
+        signUpPage.usernameField().type('1234');
+        signUpPage.passwordField().click().type('1234');
+        signUpPage.confirmPasswordField().click().type('1234');
+        signUpPage.submitButton().click();
+        signUpPage.signInTitle().should('contain', 'Sign in')
     });
 
     it('Sign in Redirection', () => {
-        signUpPage.CheckRedirection();
+        signUpPage.signInLink().click();
+        signUpPage.signInTitle().should('contain', 'Sign in');
     });
 });
 
@@ -42,29 +79,52 @@ describe ('Sign In tests', () => {
         signInPage.navigate()
     })
 
+    it ('show mock data', () => {
+        cy.intercept('POST', 'login', {fixture: 'userData.json'});
+        signInPage.usernameField().click().type('1234');
+        signInPage.passwordField().click().type('1234');
+        signInPage.submitButton().click();
+        signInPage.loggedInUserTitle().should('contain','Get Started with Real World App');
+    })
+
     it('Elements existance', () => {
-        signInPage.CheckHeaderElementExistence();
-        signInPage.CheckFieldsExistence();
-        signInPage.CheckButtonsExistence();
+        signInPage.logo().should('exist');
+        signInPage.signInTitle().should('exist');
+        signInPage.usernameField().should('exist');
+        signInPage.passwordField().should('exist');
+        signInPage.remmemberMeBox().should('exist');
+        signInPage.submitButton().should('exist');
+        signInPage.signUpLink().should('exist');
     });
     
     it('Username is required error', () => {
-        signInPage.CheckErrors('Username is required error');
+        signInPage.page().click();
+        signInPage.UsernameHelperText().should('contain', 'Username is required');
     });
 
     it('Password error', () => {
-        signInPage.CheckErrors('Password error');
+        signInPage.usernameField().click().type('1234');
+        signInPage.passwordField().click().type('123');
+        signInPage.signInTitle().click();
+        signInPage.passwordHelperText().should('contain', 'Password must contain at least 4 characters');
     });
 
     it('Sign UP Redirection', () => {
-        signInPage.CheckRedirection();
+        signInPage.signUpLink().click();
+        signInPage.signUpTitle().should('contain', 'Sign Up');
     });
 
     it('Log in', () => {
-        signInPage.LogIn();
+        signInPage.usernameField().click().type('1234');
+        signInPage.passwordField().click().type('1234');
+        signInPage.submitButton().click();
+        signInPage.loggedInUserTitle().should('contain','Get Started with Real World App');
     });
 
     it('Log in error', () => {
-        signInPage.CheckErrors('Log in error');
+        signInPage.usernameField().click().type('1235');
+        signInPage.passwordField().click().type('1235');
+        signInPage.submitButton().click();
+        signInPage.errorMessage().should('contain', 'Username or password is invalid');
     });
 });
--- cypress/page-objects/page.js
+++ cypress/page-objects/page.js
@@ -3,77 +3,65 @@ export class SignUpPage {
         cy.visit('signup');
     }
 
-    CheckHeaderElementExistence () {
-        cy.get('.makeStyles-logo-3').should('exist');
-        cy.get('[data-test="signup-title"]').should('exist');
+    logo (){
+        return cy.get('.makeStyles-logo-3');
     }
 
-    CheckFieldsExistence () {
-        cy.get('#firstName').should('exist');
-        cy.get('#lastName').should('exist');
-        cy.get('#username').should('exist');
-        cy.get('#password').should('exist');
-        cy.get('#confirmPassword').should('exist');
+    signupTitle (){
+        return cy.get('[data-test="signup-title"]');
     }
 
-    CheckButtonsExistence () {
-        cy.get('.MuiGrid-root > a').should('exist');
-        cy.get('[data-test="signup-submit"]').should('exist');
+    firstNameField(){
+        return cy.get('#firstName');
     }
 
-    clickAllFields () {
-        cy.get('#firstName').click();
-        cy.get('#lastName').click();
-        cy.get('#username').click();
-        cy.get('#password').click();
-        cy.get('#confirmPassword').click();
-        cy.get('.makeStyles-root-1').click();
+    lastNameField(){
+        return cy.get('#lastName');
     }
 
-    CheckErrorMessagesExistence () {
-        cy.get('#firstName-helper-text').should('exist');
-        cy.get('#lastName-helper-text').should('exist');
-        cy.get('#username-helper-text').should('exist');
-        cy.get('#password-helper-text').should('exist');
-        cy.get('#confirmPassword-helper-text').should('exist');
+    
+    usernameField(){
+        return cy.get('#username');
     }
 
-    CheckErrorMessagesColor () {
-        cy.get('#firstName-helper-text').should('have.css', 'color').and('eq', 'rgb(244, 67, 54)');
-        cy.get('#lastName-helper-text').should('have.css', 'color').and('eq', 'rgb(244, 67, 54)');
-        cy.get('#username-helper-text').should('have.css', 'color').and('eq', 'rgb(244, 67, 54)');
-        cy.get('#password-helper-text').should('have.css', 'color').and('eq', 'rgb(244, 67, 54)');
-        cy.get('#confirmPassword-helper-text').should('have.css', 'color').and('eq', 'rgb(244, 67, 54)');
+    passwordField(){
+        return cy.get('#password');
     }
 
-    InsertData (tipe) {
-        cy.get('#firstName').click().type('1234');
-        cy.get('#lastName').click().type('1234');
-        cy.get('#username').click().type('1234');
-
-        switch (tipe) {
-            case "Password match error":
-                cy.get('#password').click().type('1234');
-                cy.get('#confirmPassword').click().type('1111');
-                cy.get('#confirmPassword-helper-text').should('contain', 'Password does not match')
-                break;
-            case "Invalid Password error":
-                cy.get('#password').click().type('123');
-                cy.get('#confirmPassword').click().type('123');
-                cy.get('#password-helper-text').should('contain', 'Password must contain at least 4 characters')
-                break;
-            case "Registration":
-                cy.get('#password').click().type('1234');
-                cy.get('#confirmPassword').click().type('1234');
-                cy.get('[data-test="signup-submit"]').click();
-                cy.get('.MuiTypography-h5').should('contain', 'Sign in')
-                break;
-        }
+    confirmPasswordField(){
+        return cy.get('#confirmPassword');
     }
 
-    CheckRedirection () {
-        cy.get('.MuiGrid-root > a').click();
-        cy.get('.MuiTypography-h5').should('contain', 'Sign in');
+    signInLink(){
+        return cy.get('.MuiGrid-root > a');
+    }
+
+    submitButton(){
+        return cy.get('[data-test="signup-submit"]');
+    }
+
+    firstNameHelperText(){
+        return cy.get('#firstName-helper-text');
+    }
+
+    UsernameHelperText(){
+        return cy.get('#username-helper-text');
+    }
+
+    lastNameHelperText(){
+        return cy.get('#lastName-helper-text');
+    }
+
+    passwordHelperText(){
+        return cy.get('#password-helper-text');
+    }
+
+    confirmPasswordHelperText(){
+        return cy.get('#confirmPassword-helper-text');
+    }
+
+    signInTitle(){
+        return cy.get('.MuiTypography-h5');
     }
 }
 
@@ -82,55 +70,56 @@ export class SignInPage {
         cy.visit('signin');
     }
 
-    CheckHeaderElementExistence () {
-        cy.get('.makeStyles-logo-3').should('exist');
-        cy.get('.MuiTypography-h5').should('exist');
+    logo(){
+        return cy.get('.makeStyles-logo-3');
     }
 
-    CheckFieldsExistence () {
-        cy.get('#username').should('exist');
-        cy.get('#password').should('exist');
-
+    signInTitle(){
+        return cy.get('.MuiTypography-h5');
     }
 
-    CheckButtonsExistence () {
-        cy.get('.MuiFormControlLabel-root').should('exist');
-        cy.get('[data-test="signin-submit"]').should('exist');
-        cy.get('[data-test="signup"]').should('exist');
+    usernameField(){
+        return cy.get('#username');
     }
 
-    CheckErrors (tipe) {
-        switch (tipe) {
-            case "Username is required error":
-                cy.get('.makeStyles-root-1').click();
-                cy.get('#username-helper-text').should('contain', 'Username is required');
-                break;
-            
-            case "Password error":
-                cy.get('#username').click().type('1234');
-                cy.get('#password').click().type('123');
-                cy.get('.MuiTypography-h5').click();
-                cy.get('#password-helper-text').should('contain', 'Password must contain at least 4 characters');
-                break;
-
-            case "Log in error":
-                cy.get('#username').click().type('1235');
-                cy.get('#password').click().type('1235');
-                cy.get('[data-test="signin-submit"]').click();
-                cy.get('[data-test="signin-error"]').should('contain', 'Username or password is invalid');
-                break;
-        }
+    passwordField(){
+        return cy.get('#password');
     }
 
-    CheckRedirection () {
-        cy.get('.MuiGrid-root > a').click();
-        cy.get('.MuiTypography-h5').should('contain', 'Sign Up');
+    remmemberMeBox(){
+        return cy.get('.MuiFormControlLabel-root');
     }
 
-    LogIn () {
-        cy.get('#username').click().type('1234');
-        cy.get('#password').click().type('1234');
-        cy.get('[data-test="signin-submit"]').click();
-        cy.get('[data-test="user-onboarding-dialog-title"] > .MuiTypography-root').should('contain','Get Started with Real World App');
+    submitButton(){
+        return cy.get('[data-test="signin-submit"]');
     }
+
+    signUpLink(){
+        return cy.get('[data-test="signup"]');
+    }
+
+    page(){
+        return cy.get('.makeStyles-root-1');
+    }
+
+    UsernameHelperText(){
+        return cy.get('#username-helper-text');
+    }
+
+    passwordHelperText(){
+        return cy.get('#password-helper-text');
+    }
+
+    signUpTitle(){
+        return cy.get('.MuiTypography-h5');
+    }
+
+    loggedInUserTitle(){
+        return cy.get('[data-test="user-onboarding-dialog-title"] > .MuiTypography-root');
+    }
+
+    errorMessage(){
+        return cy.get('[data-test="signin-error"]');
+    }
+
 }
